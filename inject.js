93715c7246fa747dd7cd239e6e87e2c49632c747
Egor Homakov <homakov@gmail.com>
2015-03-21 23:29:47 -0700

    1

--- inject.js
+++ inject.js
@@ -1,11 +1,11 @@
 id = 9245370;
 target = 'https://gist.github.com/homakov/b9d351e4e40274fa6a40';
-if (document.domain == 'news.ycombinator.com') {
+try{
     a = new XMLHttpRequest;
     a.open('get', 'https://news.ycombinator.com/item?id=' + id, false);
     a.send();
     auth = (a.responseText.split('auth=')[1].split('&')[0]);
     location = 'https://news.ycombinator.com/vote?for=' + id + '&dir=up&auth=' + auth + '&goto=' + encodeURIComponent(target);
-} else {
+}catch(e){
     location = target;
 }
